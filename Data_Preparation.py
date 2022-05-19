eee964391f0490a265bf035a4436a896ad30f97d
__.:Kuror0:.__ <limaalyson@hotmail.com>
2022-05-18 23:04:05 -0300

    Update Data_Preparation.py

--- Data_Preparation.py
+++ Data_Preparation.py
@@ -6,15 +6,33 @@ def rmv_itens(original , exclude_list ) :
     for i in exclude_list :
         original.remove(i)
     return original
+def Separate_Train_test( path_data_dir , test_rate = 0.2 ):# , not_consider_list = [] ):
+    ark_names = os.listdir(path_data_dir)
+    test_set  = rd.sample(ark_names , int(test_rate * len(ark_names)))
+    rmv_itens(ark_names , test_set)
+    try:
+        os.makedirs(f"{path_data_dir}//Train")
+        os.makedirs(f"{path_data_dir}//Test")
+    except OSError:
+        print("Ocorreu um erro ao criar as pastas")
+    for f in ark_names :
+        shutil.move(path_data_dir + f"//{f}" ,path_data_dir + f"//Train" )
+    for f in test_set :
+        shutil.move(path_data_dir + f"//{f}" ,path_data_dir + f"//Test" )
 
+        
 
 path_Dataset_base ="C://Users//limaa//PythonProjects//VsCodePython//Processo Seletivo Docket Brasil//Dataset"
 path_simplest_rg  = path_Dataset_base + "//Simplest//RG"
+path_simplest_cnh = path_Dataset_base + "//Simplest//CNH"
+path_simplest_cpf = path_Dataset_base + "//Simplest//CPF"
 
-rg_original = os.listdir(path_simplest_rg)
+
+path_in_use = path_simplest_cpf
+rg_original = os.listdir(path_in_use)
 # rg_original.remove("Train")
 # rg_original.remove("Test")
-rg_down_Samples = rd.sample(rg_original ,  3)
+# rg_down_Samples = rd.sample(rg_original ,  3)
 rmv_list = rd.sample(rg_original ,  55)
 if "Train" in rmv_list :
     rmv_list += rd.sample(rg_original ,  1)
@@ -31,7 +49,7 @@ print(len(rg_original))#rg_original)
 
 """#REMOVE ARQUIVOS PARA FAZER O BALANCIAMENTO
 for i in rmv_list :
-    path = path_simplest_rg + f"//{i}"
+    path = path_in_use + f"//{i}"
     ## If file exists, delete it ##
     if os.path.isfile(path):
         os.remove(path)
@@ -40,3 +58,4 @@ for i in rmv_list :
     else:    ## Show an error ##
         print("Error: %s file not found" % path)"""
 
+Separate_Train_test(path_in_use)
